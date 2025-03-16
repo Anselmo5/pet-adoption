67f215debc7c5e8de60eaf4b2af129b3de3229ca
Anselmo5 <anselmohenrique191@gmail.com>
2025-03-16 19:38:58 -0300

    update

--- Pet/app/(tabs)/index.tsx
+++ Pet/app/(tabs)/index.tsx
@@ -1,73 +1,124 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import { useState } from 'react';
-export default function App() {
-  const [ocultarSenha, setOcultarSenha] = useState(true);
+import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
+
+export default function App() { // navigation é passado como prop
   return (
     <View style={styles.container}>
-      
-      <View style={styles.LogoContainer}>
-          <Text>LOGIN</Text>
-          <Text>Insira seus dados para continuar</Text>
-      </View>
+      <View style={styles.rowContainer}>
+        <View style={styles.LogoContainer}>
+          <Image 
+            source={require('../../assets/images/ico.png')} 
+            style={styles.Logo} 
+          />
+          <Text style={styles.loginText}>LOGIN</Text>
+          <Text style={styles.subText}>Insira seus dados para continuar</Text>
+        </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.title}>EMAIL</Text>
-        <TextInput
-          style={styles.input}
-          placeholder='user@exemplo.com.br'
-        />
-        <Text style={styles.title}>SENHA</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry={ocultarSenha}
-          
-        />
-        <TouchableOpacity style={styles.botao}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.title}>EMAIL</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='user@exemplo.com.br'
+          />
+
+          <Text style={styles.title}>SENHA</Text>
+          <View style={styles.senhaContainer}>
+            <TextInput
+              style={styles.inputSenha}
+              secureTextEntry={true}
+              placeholder="Digite sua senha"
+            />
+          </View>
+
+          <TouchableOpacity style={styles.botao}>
             <Text style={styles.textoBotao}>Entrar</Text>
-        </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
-    LogoContainer:{
-      margin:5,
-      padding:10,
-    },
-    input:{
-      padding:10,
-      margin:1,
-      borderRadius: 2,
-      marginBottom: 10, // Espaçamento entre os itens
-      borderBottomWidth: 2, // Espessura da borda inferior
-      borderBottomColor: 'black'
-    },
-
-    botao:{
-      padding:10,
-      textAlign:'center',
-      marginTop:5,
-      backgroundColor:'red',
-      borderRadius: 3,
-    },
-
-    textoBotao:{
-      textAlign:'center',
-    },
-
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f5f5f5',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
+
+  rowContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 10,
+  },
+
+  LogoContainer: {
+    alignItems: 'flex-start',
+    marginRight: 30,
+    marginBottom: 30,
+  },
+
+  Logo: {
+    width: 40,
+    height: 40,
     marginBottom: 10,
+  },
+
+  loginText: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginBottom: 5,
+  },
+
+  subText: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#9D9E9F',
+  },
+
+  inputContainer: {
+    flex: 1,
+    marginBottom: 50,
+  },
+
+  input: {
+    padding: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    marginBottom: 10,
+  },
+
+  senhaContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  inputSenha: {
+    flex: 1,
+    padding: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    marginBottom: 10,
+  },
+
+  botao: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#B63BD9',
+    borderRadius: 65,
+    alignItems: 'center',
+  },
+
+  textoBotao: {
+    color: '#FCF5FD',
+    fontWeight: 'bold',
+  },
+
+  title: {
+    fontSize: 10,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#DA77EC',
   },
 });
